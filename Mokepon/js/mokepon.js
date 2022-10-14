60aa1201a1e950d25a288ef6bf0e706f6cf870a7
Gabriela Granda <gabrielagrandatarrio@gmail.com>
2022-10-14 10:19:00 -0400

    clase 43

--- Mokepon/js/mokepon.js
+++ Mokepon/js/mokepon.js
@@ -4,8 +4,14 @@ let vidasJugador = 3
 let vidasEnemigo = 3
 
 function iniciarJuego(){
+    let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+    sectionSeleccionarAtaque.style.display = "none"
+
+    let sectionReiniciar = document.getElementById("reiniciar")
+    sectionReiniciar.style.display = "none"
+
     let botonMascotaJugador = document.getElementById("boton-mascota")
-botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+    botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
 
     let botonFuego = document.getElementById("boton-fuego")
     botonFuego.addEventListener("click", ataqueFuego)
@@ -19,6 +25,12 @@ botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
 }
 
 function seleccionarMascotaJugador(){
+    let sectionSeleccionarMascota = document.getElementById("seleccionar-mascota")
+    sectionSeleccionarMascota.style.display = "none"
+    
+    let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+    sectionSeleccionarAtaque.style.display = "block"
+
     let inputHipodoge = document.getElementById("hipodoge")
     let inputCapipepo = document.getElementById("capipepo")
     let inputRatigueya = document.getElementById("ratigueya")
@@ -137,6 +149,9 @@ function CrearMensajeFinal(resultadoFinal){
     botonAgua.disabled = true
     let botonTierra = document.getElementById("boton-tierra")
     botonTierra.disabled = true
+
+    let sectionReiniciar = document.getElementById("reiniciar")
+    sectionReiniciar.style.display = "block"
 }
 
 function reiniciarJuego(){
